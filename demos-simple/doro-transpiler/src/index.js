ad0dffb6ba835d553b9a43e59e78d26ee2cea3b1
Fallenstar <ringanaro@gmail.com>
2025-10-20 16:36:07 +0300

    Split doro-transpiler

--- demos-simple/doro-transpiler/src/index.js
+++ demos-simple/doro-transpiler/src/index.js
@@ -1,28 +1,12 @@
-const doroUrl = "https://i.ibb.co/Xxww9JT3/fallen-doro.png";
-const doroCache = {
-  blob: null,
-  blobUrl: "",
-  imgs: [],
-};
+const imgs = [];
 const maxDoroCount = 4;
 
 const container = document.querySelector(".wrapper");
 
-async function loadImage() {
-  const img = document.createElement("img");
-  if (doroCache.blobUrl === "" || doroCache.blob === null) {
-    const resp = await fetch(doroUrl);
-    if (!resp.ok) throw new Error("Failed to load image!");
-    doroCache.blob = await resp.blob();
-    doroCache.blobUrl = URL.createObjectURL(doroCache.blob);
-  }
-
-  img.src = doroCache.blobUrl;
-  img.alt = "fallen-doro";
-  img.onload = () => (img.classList = "loaded");
-  doroCache.imgs.push(img);
-
-  if (doroCache.imgs.length <= maxDoroCount) container.appendChild(img);
+async function loadImageAsync() {
+  const img = await createImgElementAsync();
+  imgs.push(img);
+  if (imgs.length <= maxDoroCount) container.appendChild(img);
   else {
     const button = document.querySelector("button");
     button.textContent = "There are too many doros!";
